0e362aa86554426070f62f31b82bbe8cd548d0db
Vaidas <61739600+Vaidelis@users.noreply.github.com>
2022-04-06 20:29:39 +0200

    deleted not used script in iteminformation.blade.php

--- resources/views/itemInformation.blade.php
+++ resources/views/itemInformation.blade.php
@@ -154,21 +154,6 @@
                 MainImg.src = smallimg[i].src;
             }
         })(i);
-        /*smallimg[0].onclick = function (){
-            MainImg.src = smallimg[0].src;
-        }
-        smallimg[1].onclick = function (){
-            MainImg.src = smallimg[1].src;
-        }
-        smallimg[2].onclick = function (){
-            MainImg.src = smallimg[2].src;
-        }
-        smallimg[3].onclick = function (){
-            MainImg.src = smallimg[3].src;
-        }
-        smallimg[4].onclick = function (){
-            MainImg.src = smallimg[4].src;
-        }*/
     </script>
         </body>
 @endsection
