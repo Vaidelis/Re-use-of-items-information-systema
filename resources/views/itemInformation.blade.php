93a783a0b182075715fa3b53387e521f1df26232
Vaidas <61739600+Vaidelis@users.noreply.github.com>
2022-04-10 16:50:24 +0200

    Design for item announcement page and pagination for saved pins

--- resources/views/itemInformation.blade.php
+++ resources/views/itemInformation.blade.php
@@ -20,7 +20,7 @@
                 <form method="POST" action="{{route('itemforget', $item->id)}}" id="deleteForm">
                     @csrf
                     @method('DELETE')
-                    <button <?php if($remember == null){ ?> hidden <?php }?> type="submit">Pamiršti</button>
+                 <button class="btn btn-primary btn-xl js-scroll-trigger" style="cursor: pointer; overflow: auto" <?php if($remember == null){ ?> hidden <?php }?> type="submit">Pamiršti</button>
                 </form>
     </div>
     <hr>
@@ -64,6 +64,9 @@
                 <p style="font-weight: bold">Aprašymas</p>
                 <p>{{ $item->information }}<p>
                 <a style="height: 40px; margin-top:auto; margin-bottom: auto;" href="{{route('itembuy', ['id' => $item->id, 'userid' => $item->user_id])}}"><button class="btn btn-primary btn-xl js-scroll-trigger" <?php if($bought != null || Auth::User()->id == $item->user_id){ ?> disabled <?php }?>  class="">Pirkti daiktą</button></a>
+                   <br>
+                    <br>
+                    <a href="#services" class="js-scroll-trigger"><button class="btn btn-primary btn-xl js-scroll-trigger">Perdirbimo paslaugų pasiūlymai</button></a>
             </div>
         </div>
 
@@ -71,6 +74,7 @@
     <hr>
     <div class="container2">
         <div class="block_container">
+            
         @foreach($pins as $pin)
         <div class="container col-md-6">
         <a data-pin-do="embedPin" data-pin-save="false" href="https://www.pinterest.com/pin/{{$pin['id']}}/"></a>
@@ -97,49 +101,88 @@
                     {!! $pins->links() !!}
                 </div>
             </div>
+
+    </div>
+    <div style="clear:both;">
+        <hr>
     </div>
 
-    <div style="clear:both;" class="container">
-        <hr>
-            <p class="">Paslaugų perdirbimo pasiūlymai</p>
-            <tr class="">Paslaugos pavadinimas</tr>
-            <tr class="">Įsiminti</tr>
-            <br>
+    <div style="clear:both;" class="row" id="services">
+
+        <div class="col-lg-5 col-md-12 col-12">
+        <table class="content-table">
+            <thead>
+            <th colspan="2" style="text-align: center">Paslaugų perdirbimo pasiūlymai</th>
+            </thead>
+            <thead>
+            <th>Paslaugos pavadinimas</th>
+            <th>Įsiminti</th>
+            </thead>
+            <tbody>
             @foreach($service as $serv)
                 @if($serv->service->hide == 0)
-               <a class="" href="{{route('serviceshow', $serv->service->id)}}">{{$serv->service->name}}</a>
-                    <a href="{{route('saveserviceinitem', ['id' => $item->id, 'servid' => $serv->service->id])}}"><button
-                        <?php foreach($itemhasservice as $itemhas){
+                    <tr>
+                    <td><a class="" href="{{route('serviceshow', $serv->service->id)}}">{{$serv->service->name}}</a></td>
+                   <td> <a href="{{route('saveserviceinitem', ['id' => $item->id, 'servid' => $serv->service->id])}}"><button class="btn btn-primary btn-xl js-scroll-trigger"
+                            <?php foreach($itemhasservice as $itemhas){
                             if($serv->services_announcement_id != $itemhas->services_announcement_id){
                             }
                             else{
-
-
-                         ?> disabled
-                        <?php }} ?> >Įsiminti</button></a>
-                    <br>
+                            ?> disabled
+                        <?php }} ?> >Įsiminti</button></a></td>
+                    </tr>
                 @endif
             @endforeach
-            <p>Išsaugotos paslaugos</p>
-            @foreach($itemhasservice as $serv)
-                @if($serv->service->hide == 0)
-                    <div style="display: flex; flex-direction: row;">
-                    <a class="" href="{{route('serviceshow', $serv->service->id)}}">{{$serv->service->name}}</a>
-                    <form method="POST" action="{{route('itemnoservice', ['id' => $item->id, 'someid' => $serv->id])}}" id="deleteForm">
-                        @csrf
-                        @method('DELETE')
-                        <a><button type="submit">Pamiršti</button></a>
-                    </form>
-                    </div>
-                    <br>
-                @endif
-            @endforeach
-            <p>Išsaugoti pinai</p>
-            <div>
-            @foreach($itemhaspins as $pins)
-                    <a data-pin-do="embedPin" href="https://www.pinterest.com/pin/{{$pins->pinpicture}}/"></a>
+            </tbody>
+        </table>
+        </div>
+        <div class="col-lg-6 col-md-12 col-12">
+            <table class="content-table">
+                <thead>
+                <th colspan="2" style="text-align: center">Išsaugoti Paslaugų perdirbimo pasiūlymai</th>
+                </thead>
+                <thead>
+                <th>Išsaugotos paslaugos</th>
+                <th>Pamiršti</th>
+                </thead>
+                <tbody>
+                @foreach($itemhasservice as $serv)
+                    @if($serv->service->hide == 0)
+                        <tr>
+                          <td> <a class="" href="{{route('serviceshow', $serv->service->id)}}">{{$serv->service->name}}</a></td>
+                           <td> <form method="POST" action="{{route('itemnoservice', ['id' => $item->id, 'someid' => $serv->id])}}" id="deleteForm">
+                                @csrf
+                                @method('DELETE')
+                                <a><button class="btn btn-primary btn-xl js-scroll-trigger" type="submit">Pamiršti</button></a>
+                            </form> </td>
+                        </tr>
+                    @endif
                 @endforeach
+                </tbody>
+            </table>
+
+        </div>
+        <div style="clear:both" class="col-lg-7 col-md-10 col-8">
+            <table class="content-table">
+                <thead>
+                <th style="text-align: center">Išsaugoti pinai</th>
+                </thead>
+                <tbody class="row">
+                    @foreach($itemhaspins as $pins)
+                        <td class="container col-md-6">
+                        <a data-pin-do="embedPin" href="https://www.pinterest.com/pin/{{$pins->pinpicture}}/"></a>
+                        </td>
+                    @endforeach
+
+                </tbody>
+            </table>
+            {{-- Pagination --}}
+            <div class="" name="action" value='html' style="float:left">
+                <div class="bottom">
+                    {!! $itemhaspins->links() !!}
+                </div>
             </div>
+        </div>
     </div>
 
 
@@ -149,11 +192,17 @@
         var smallimg = document.getElementsByClassName('img-fluid');
         var all = 5;
 
-        for ( var i = 0; i < 5; i++ ) (function(i){
+        for ( var i = 0; i < 12; i++ ) (function(i){
             smallimg[i].onclick = function (){
                 MainImg.src = smallimg[i].src;
             }
         })(i);
     </script>
+
+    <!-- Plugin JavaScript -->
+    <script src="/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Custom scripts for this template -->
+    <script src="/js/creative.min.js"></script>
         </body>
 @endsection
