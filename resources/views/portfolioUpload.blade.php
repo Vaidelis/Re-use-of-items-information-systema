4b0619f963a111e12cdb03b9048e304a9c7136eb
Vaidas <61739600+Vaidelis@users.noreply.github.com>
2022-04-10 20:35:19 +0200

    Portfolio upload design

--- resources/views/portfolioUpload.blade.php
+++ resources/views/portfolioUpload.blade.php
@@ -19,24 +19,32 @@
         </div>
         <label class="label">Daiktas prieš perdarymą: </label>
         <div class="form-group">
-            <input type="file" name="image" class="form-control" accept="image" required>
+            <label>
+                <img style="height: 50px; width: 50px" src="/img/picture.svg.png">
+            <input type="file" name="image" style="display: none" class="form-control" accept="image/*" onchange="loadFile(event)" required>
             @if ($errors->has('files'))
                     <span class="invalid-feedback" role="alert">
                                 <strong>{{ $error }}</strong>
                                      </span>
             @endif
+            </label>
+            <img id="output" style="width: 200px; height: 200px; object-fit: cover;"/>
         </div>
         <label class="label">Daiktas po perdarymą: </label>
         <div class="form-group">
-            <input type="file" name="image2" class="form-control" accept="image" required>
+            <label>
+                <img style="height: 50px; width: 50px" src="/img/picture.svg.png">
+            <input type="file" name="image2" style="display: none" class="form-control" accept="image/*" onchange="loadFile2(event)" required>
             @if ($errors->has('files'))
                 <span class="invalid-feedback" role="alert">
                                 <strong>{{ $error }}</strong>
                                      </span>
             @endif
+            </label>
+            <img id="output2" style="width: 200px; height: 200px; object-fit: cover;"/>
         </div>
         <div class="form-group">
-            <input type="submit" class="btn btn-success" />
+            <input value="Įkelti" type="submit" class="btn btn-success" />
             <a href="{{ route('portfolioshow', $id) }}" class="btn btn-primary">Atgal</a>
         </div>
     </form>
@@ -47,6 +55,15 @@
         </div>
     </div>
 
-
+    <script>
+        var loadFile = function(event) {
+            var image = document.getElementById('output');
+            image.src = URL.createObjectURL(event.target.files[0]);
+        };
+        var loadFile2 = function(event) {
+            var image = document.getElementById('output2');
+            image.src = URL.createObjectURL(event.target.files[0]);
+        };
+    </script>
 
 @endsection
